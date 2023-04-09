0ff47862b970126457005d7c875b3815d55b0d7a
4ngryn3rds <@gmail.com>
2023-04-09 21:52:35 +0300

    v0.1.2 header reworked

--- src/burger.js
+++ src/burger.js
@@ -1,23 +1,23 @@
-function burgerClick() {
-    console.log("evetnt_detected")
-    if  (getComputedStyle(document.getElementById("burger-menu")).display == "none") {
-        document.getElementById("header__menu").src = "../assets/images/menu-close.svg";
-        openMenu();
-    } else if (getComputedStyle(document.getElementById("burger-menu")).display == "flex") {
-        document.getElementById("header__menu").src = "../assets/images/menu.svg";
-        closeMenu();
+// function burgerClick() {
+//     console.log("evetnt_detected")
+//     if  (getComputedStyle(document.getElementById("burger-menu")).display == "none") {
+//         document.getElementById("header__menu").src = "../assets/images/menu-close.svg";
+//         openMenu();
+//     } else if (getComputedStyle(document.getElementById("burger-menu")).display == "flex") {
+//         document.getElementById("header__menu").src = "../assets/images/menu.svg";
+//         closeMenu();
 
-    }
+//     }
 
-}
+// }
 
-function openMenu() {
-    let burger = document.getElementById("burger-menu");
-    burger.style.display = "flex";
-}
+// function openMenu() {
+//     let burger = document.getElementById("burger-menu");
+//     burger.style.display = "flex";
+// }
 
-function closeMenu() {
-    let burger = document.getElementById("burger-menu");
-    burger.style.display = "none";
-}
+// function closeMenu() {
+//     let burger = document.getElementById("burger-menu");
+//     burger.style.display = "none";
+// }
 
